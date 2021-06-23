67ec4672e58e8364e15f0e7ac8a421c0c4b105eb
Abdull243 <80228657+Abdull243@users.noreply.github.com>
2021-06-22 21:26:56 -0500

    Update Mylinuxscript.sh

--- Linux/Mylinuxscript.sh
+++ Linux/Mylinuxscript.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+cd /03-student/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis
+cat 0310_Dealer_schedule | grep "05:00:00 AM\|08:00:00 AM\|02:00:00 PM\|08:00:00 PM\|11:00:00 PM" | awk -F" " '{print $1,$2,$5,$6}' >> Dealers_working_during_losses.txt
+cat 0312_Dealer_schedule | grep "05:00:00 AM\|08:00:00 AM\|02:00:00 PM\|08:00:00 PM\|11:00:00 PM" | awk -F" " '{print $1,$2,$5,$6}' >> Dealers_working_during_losses.txt
+cat 0315_Dealer_schedule | grep "05:00:00 AM\|08:00:00 AM\|02:00:00 PM" | awk -F" " '{print $1,$2,$5,$6}' >> Dealers_working_during_losses.txt
 
